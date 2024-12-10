1bbe681bae61c5e237ca1d5628737e4babec60c2
itsnotm3h <thewhit3pages@gmail.com>
2024-12-11 00:14:38 +0800

    updated-2024

--- js/script.js
+++ js/script.js
@@ -2,20 +2,23 @@ let info = [
     {
         name: "This Portfolio",
         summary: "A website about me.",
-        technology: ["Javascript","Html/Css","jQuery",,"Bootstrap"],
-        github: "https://github.com/itsnotm3h/trip-expense-logging-system",
+        image: "./img/portfoilo.png",
+        technology: ["Javascript", "Html/Css", "jQuery", , "Bootstrap"],
+        github: "https://github.com/itsnotm3h/portfolio",
         demo: ""
     },
     {
         name: "Behind the Painting",
         summary: "Users can write/expand stories inspired by paintings.",
-        technology: ["Axios","Javascript","Html/Css","Bootstrap"],
-        github: "https://github.com/itsnotm3h/CRUD-assignment-art-story-contest/deployments",
+        image: "./img/artContest.png",
+        technology: ["Axios", "Javascript", "Html/Css", "Bootstrap"],
+        github: "https://github.com/itsnotm3h/CRUD-assignment-art-story-contest/",
         demo: "https://itsnotm3h.github.io/CRUD-assignment-art-story-contest/"
     },
     {
         name: "Trip Expense Logging System",
         summary: "An command line interface application to log, calculate and print trip expenses.",
+        image: "./img/inteliJ.png",
         technology: ["Java"],
         github: "https://github.com/itsnotm3h/trip-expense-logging-system",
         demo: ""
@@ -23,25 +26,26 @@ let info = [
     {
         name: "Restful.Api Routes",
         summary: "Creating routes to CRUD expenses in MongoDB database.",
-        technology: ["Restful.api","MongoDb","Express","Javascript"],
+        image: "./img/mongoDB.png",
+        technology: ["Restful.api", "MongoDb", "Express", "Javascript"],
         github: "https://github.com/itsnotm3h/database-RESTFUL",
         demo: ""
     },
     {
-        name: "ITM Academy Attendance",
-        summary: "Designing and application of MYSQL database to create a attendance taking system.",
-        technology: ["Express.js","Handlebars","Javascript","MySQL"],
-        github: "https://github.com/itsnotm3h/msql-assignment/tree/main",
-        demo: ""
+        name: "PokeQuiz",
+        summary: "Explore map and capture Pokemon by answering a math quizzes to catch 'em all!",
+        image: "./img/pokeQuiz.png",
+        technology: ["Javascript", "Bootstrap", "Leaflet", "PokeAPI"],
+        github: "https://github.com/itsnotm3h/industryproject",
+        demo: "https://itsnotm3h.github.io/industryproject/"
     }
-    // { name: "Note Taking", summary: "Bootcamp Project", technology: [""], github: "", demo: "" },
-    // { name: "Data", summary: "Bootcamp Project", technology: [""], github: "", demo: "" },
-    // { name: "Eccomerce", summary: "Industry Project", technology: ["Html/Css", "React.js", "Bootstrap", "Express"], github: "", demo: "" },
-    // { name: "PokeQuiz", summary: "Industry Project", technology: ["Html/Css", "Axios", "Bootstrap"], github: "", demo: "" },
 ]
 
 $(".projectName").html(info[0].name);
 $(".projectSummary").html(info[0].summary);
+$(".github").off("click").on("click", function () {
+    window.open(info[0].github);
+});
 
 
 
@@ -67,25 +71,67 @@ $(".projectSummary").html(info[0].summary);
 // }
 
 
+function setCarousell(item, index) {
+
+    let html = "";
+    let active = "";
+
+    if (index == 0) {
+        active = "active";
+    }
+
+    html = `<div class="carousel-item ${active}" data-bs-toggle="modal" data-bs-target="#exampleModal" id="slide${index + 1}">
+    <a href="${item.github}" target="_blank"><img src="${item.image}" class="img-fluid" alt="${item.name}"></a>
+    </div>`;
+
+    $(".carousel-inner").append(html);
+
+}
+
+const technology = info.map(setCarousell);
+
+
+
+
 
 const myCarousel = document.getElementById('projectCarousel')
 
 myCarousel.addEventListener('slide.bs.carousel', event => {
     let currentIndex = event.to;
     let tagLibrary = info[currentIndex].technology;
-    let tagsHtml="";
+    let tagsHtml = "";
     $(".projectName").html(info[currentIndex].name);
     $(".projectSummary").html(info[currentIndex].summary);
 
-    for(let x in tagLibrary)
-    {
-        tagsHtml +=`<div class="tag">${tagLibrary[x]}</div>`
+    for (let x in tagLibrary) {
+        tagsHtml += `<div class="tag">${tagLibrary[x]}</div>`
     }
 
     $(".projectTags").html(tagsHtml);
 
+    $(".github").off("click").on("click", function () {
+        window.open(info[currentIndex].github);
+        console.log(info[currentIndex].github);
+    });
 
-    let tech = info[currentIndex].technology;
+    let demoURL = info[currentIndex].demo;
+
+    if (demoURL != "") {
+
+        $(".demo").removeClass("d-none");
+        $(".demo").off("click").on("click", function () {
+            window.open(demoURL);
+            console.log(demoURL);
+
+        })
+    }
+    else {
+        $(".demo").addClass("d-none");
+    }
+
+
+
+    // let tech = info[currentIndex].technology;
 
 
     //use hmap to display the different tech used.
